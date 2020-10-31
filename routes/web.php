13f491a13fb614a1624e414a78fc65a77f97b155
Eden Ramoneda <edenlee143@gmail.com>
2020-10-31 23:14:27 +0800

    added chat controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/chats', 'ChatController@index');
+Route::get('/messages', 'ChatController@fetchAllMessages');
+Route::post('/messages', 'ChatController@sendMessage');
